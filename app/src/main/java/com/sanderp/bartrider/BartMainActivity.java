ac4057318959a689f8ce59e62d854c1edb10a1c5
Sander Peerna <sanderpeerna@gmail.com>
2015-08-31 20:06:13 -0700

    Initial commit

---
+++ app/src/main/java/com/sanderp/bartrider/BartMainActivity.java
@@ -0,0 +1,130 @@
+package com.sanderp.bartrider;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.sanderp.bartrider.BartXmlParser.StationDeparture;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by Sander Peerna on 8/23/2015.
+ */
+public class BartMainActivity extends AppCompatActivity {
+
+    private static final String TAG = BartMainActivity.class.getSimpleName();
+
+    private List<StationDeparture> departures;
+    private TextView mDestination;
+    private TextView mPlatform;
+    private TextView mMinutes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bart_main);
+
+        mDestination = (TextView) findViewById(R.id.destination);
+        mPlatform = (TextView) findViewById(R.id.platform);
+        mMinutes = (TextView) findViewById(R.id.minutes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bart_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_refresh:
+                new BartAPISyncTask().execute("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=cast&key=MW9S-E7SL-26DU-VV8V");
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Implementation of the AsyncTask to download data from the BART API.
+     */
+    private class BartAPISyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... bartUrl) {
+            try {
+                return refreshData(bartUrl[0]);
+            } catch (IOException e) {
+                return "Failed to refresh";
+            } catch (XmlPullParserException e) {
+                return "XML parser failed";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            for (StationDeparture d : departures) {
+                mDestination.setText(d.destination);
+                mPlatform.setText(d.platform);
+                mMinutes.setText(d.minutes);
+            }
+            Log.i(TAG, result);
+        }
+    }
+
+    /**
+     * Creates the stream for AsyncTask
+     */
+    private String refreshData(String bartUrl) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        BartXmlParser parser = new BartXmlParser();
+        String data = "";
+
+        try {
+            stream = downloadData(bartUrl);
+            departures = parser.parse(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        return departures.toString();
+    }
+
+    /**
+     * Sets up a connection and gets an input stream
+     */
+    private InputStream downloadData(String bartUrl) throws IOException {
+        URL url = new URL(bartUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000);
+        conn.setConnectTimeout(15000);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+
+        conn.connect();
+        return conn.getInputStream();
+    }
+}
---
+++ app/src/main/java/com/sanderp/bartrider/BartXmlParser.java
@@ -0,0 +1,129 @@
+package com.sanderp.bartrider;
+
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sander Peerna on 8/30/2015.
+ */
+public class BartXmlParser {
+
+    private static final String TAG = BartXmlParser.class.getSimpleName();
+
+    private static final String ROOT = "root";
+    private static final String STATION = "station";
+    private static final String ESTIMATE_TIME_TO_DEPARTURE = "etd";
+    private static final String ESTIMATE = "estimate";
+    private static final String DESTINATION = "destination";
+    private static final String MINUTES = "minutes";
+    private static final String PLATFORM = "platform";
+
+    // Namespace is not used for this parser
+    public static final String ns = null;
+
+    public static class StationDeparture {
+        public final String destination;
+        public final String platform;
+        public final String minutes;
+
+        private StationDeparture(String destination, String platform, String minutes) {
+            this.destination = destination;
+            this.platform = platform;
+            this.minutes = minutes;
+        }
+    }
+
+    public List<StationDeparture> parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+
+            while (parser.next() != XmlPullParser.END_DOCUMENT) {
+                if (parser.getEventType() != XmlPullParser.START_TAG) continue;
+                if (parser.getName().equals(STATION)) break;
+            }
+
+            return readAPI(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    public List<StationDeparture> readAPI(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List<StationDeparture> departures = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, STATION);
+        while (parser.next() !=  XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) continue;
+
+            String name = parser.getName();
+            if (name.equals(ESTIMATE_TIME_TO_DEPARTURE)) {
+                departures = readEstimatedDepartures(parser);
+            }
+        }
+        return departures;
+    }
+
+    public List<StationDeparture> readEstimatedDepartures(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List<StationDeparture> departures = new ArrayList();
+        String destination = "";
+
+        parser.require(XmlPullParser.START_TAG, ns, ESTIMATE_TIME_TO_DEPARTURE);
+        while (parser.next() !=  XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) continue;
+
+            String name = parser.getName();
+            if (name.equals(DESTINATION)) {
+                destination = readTag(parser, DESTINATION);
+                Log.i(TAG, destination);
+            } else if (name.equals(ESTIMATE)) {
+                departures.add(readEstimate(parser, destination));
+            }
+        }
+        return departures;
+    }
+
+    public StationDeparture readEstimate(XmlPullParser parser, String dest) throws XmlPullParserException, IOException {
+        String destination = dest;
+        String minutes = "";
+        String platform = "";
+
+        parser.require(XmlPullParser.START_TAG, ns, ESTIMATE);
+        while (parser.next() !=  XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() == XmlPullParser.END_TAG && parser.getName().equals(ESTIMATE)) break;
+            if (parser.getEventType() != XmlPullParser.START_TAG) continue;
+
+            String name = parser.getName();
+            if (name.equals(MINUTES)) {
+                minutes = readTag(parser, MINUTES);
+                Log.i(TAG, minutes);
+            } else if (name.equals(PLATFORM)) {
+                platform = readTag(parser, PLATFORM);
+                Log.i(TAG, platform);
+            }
+        }
+        return new StationDeparture(destination, platform, minutes);
+    }
+
+    public String readTag(XmlPullParser parser, String tag) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, tag);
+
+        String value = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            value = parser.getText();
+            parser.nextTag();
+        }
+
+        parser.require(XmlPullParser.END_TAG, ns, tag);
+        return value;
+    }
+}
