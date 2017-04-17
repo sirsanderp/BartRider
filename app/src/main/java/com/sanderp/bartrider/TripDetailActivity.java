775480d2bfa13478358a9bc51595645200874f2e
Sander Peerna <sanderpeerna@gmail.com>
2017-04-16 17:26:28 -0700

    Added custom toolbar with up navigation.

--- app/src/main/java/com/sanderp/bartrider/TripDetailActivity.java
+++ app/src/main/java/com/sanderp/bartrider/TripDetailActivity.java
@@ -1,8 +1,10 @@
 package com.sanderp.bartrider;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,15 +14,29 @@ import com.sanderp.bartrider.structure.Trip;
 public class TripDetailActivity extends AppCompatActivity {
     private static final String TAG = "TripDetailActivity";
 
+    public static final String ORIG = "origin";
+    public static final String DEST = "destination";
+    public static final String TRIP = "trip";
+
+    private ListView mListView;
     private TextView mOrig;
     private TextView mDest;
     private TextView mCo2;
-    private ListView mListView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trip_detail);
+
+        // Set up Toolbar to replace ActionBar.
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("Trip Details");
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_24dp);
+        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeButtonEnabled(true);
 
         mOrig = (TextView) findViewById(R.id.trip_orig);
         mDest = (TextView) findViewById(R.id.trip_dest);
@@ -28,9 +44,9 @@ public class TripDetailActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.trip_detail_list_view);
 
         Intent data= this.getIntent();
-        Trip trip = (Trip) data.getSerializableExtra("trip");
-        mOrig.setText(data.getStringExtra("origin"));
-        mDest.setText(data.getStringExtra("destination"));
+        Trip trip = (Trip) data.getSerializableExtra(TRIP);
+        mOrig.setText(data.getStringExtra(ORIG));
+        mDest.setText(data.getStringExtra(DEST));
         mCo2.setText("CO2 Saved: " + trip.getCo2());
 
         TripLegAdapter adapter = new TripLegAdapter(TripDetailActivity.this, trip.getTripLegs());
