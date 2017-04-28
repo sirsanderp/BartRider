d085ecef3a715762be099ed56155d54f8e9dbb95
Sander Peerna <sanderpeerna@gmail.com>
2017-04-28 00:24:26 -0700

    Switched to using JSON API and Jackson POJOs.

--- app/src/main/java/com/sanderp/bartrider/xmlparser/AdvisoryParser.java
+++ app/src/main/java/com/sanderp/bartrider/xmlparser/AdvisoryParser.java
@@ -14,6 +14,7 @@ import java.util.List;
 /**
  * Returns the current BART Service Advisories (BSA).
  */
+@Deprecated
 public class AdvisoryParser {
     private static final String TAG = "AdvisoryParser";
 
